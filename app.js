56533083c7c8000313857ba0d3d3aadaae62de76
swag31415 <kidswagatam18@gmail.com>
2021-12-17 17:42:57 -0500

    Added helpful while optimizing it for node and this environment

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.get("/", (req, res) => {
   res.render("index")
 })
 
+// Stonkey
 const rand = (n) => Math.ceil(n*Math.random())
 app.get("/stonkey", (req, res) => {
   https.get("https://www.alphavantage.co/query?" + new URLSearchParams({
@@ -20,6 +21,16 @@ app.get("/stonkey", (req, res) => {
     slice: `year${rand(2)}month${rand(12)}`,
     apikey: process.env.STONKEY_KEY || "demo"
   }), resp => resp.pipe(res))
+})
+
+// Helpful
+const helpful_api = require("./src/helpful_api.js")
+app.post("/helpful", (req, res) => {
+  helpful_api.get_advice(req.body.question).then(advice => {
+    // Sort the advice and return the top 50
+    sorted = Object.keys(advice).sort((a, b) => advice[b]-advice[a])
+    res.json(sorted.slice(0, 50))
+  })
 })
 
 app.listen(port, () => {
