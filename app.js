fcda208ad67a837a88b74c980e40837389bc1af7
swag31415 <kidswagatam18@gmail.com>
2021-12-17 16:34:53 -0500

    Added support for stonkey

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express")
+const https = require("https")
 const app = express()
   .set("view engine", "ejs")
 const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
   res.render("index")
+})
+
+const rand = (n) => Math.ceil(n*Math.random())
+app.get("/stonkey", (req, res) => {
+  https.get("https://www.alphavantage.co/query?" + new URLSearchParams({
+    function: "TIME_SERIES_INTRADAY_EXTENDED",
+    symbol: req.query.sym,
+    interval: "5min",
+    slice: `year${rand(2)}month${rand(12)}`,
+    apikey: process.env.STONKEY_KEY || "demo"
+  }), resp => resp.pipe(res))
 })
 
 app.listen(port, () => {
